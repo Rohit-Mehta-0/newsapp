fd352e312cd89ef35c23b696f128c24b904d1313
rohit-mehta-0 <mehtarohit7644@gmail.com>
2023-03-23 19:33:46 +0530

    basic layout complete

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "d3a68d3a93a54948a016a1553bc4d20c";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=bitcoin&apiKey=ca674904aa984c8f931016f07013db4e`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
